ef535b84e08ca4ee31e2858c41c7302f2a1b2e60
LukaszK1983 <luke83@poczta.fm>
2019-08-14 19:21:30 +0200

    UserDao do weryfikacji

--- src/main/java/pl/coderslab/UserDAO/UserDAO.java
+++ src/main/java/pl/coderslab/UserDAO/UserDAO.java
@@ -16,14 +16,15 @@ public class UserDAO {
     private static final String FIND_ALL_USERS_QUERY = "SELECT id, name, email, password, passwordSalt FROM users";
     private static final String FIND_ALL_GROUP_USERS_QUERY = "SELECT u.id, u.name, email, password, passwordSalt FROM users u JOIN user_group ug ON u.id = ug.user_id WHERE ug.id = ?";
 
-    public User create(User user) {
+    public User create(User user) throws SQLException {
+        Connection conn = getConnection();
         try  {
-            Connection conn = getConnection();
+            int idx = 0;
             PreparedStatement statement = conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, user.getName());
-            statement.setString(2, user.getEmail());
-            statement.setString(3, user.getPassword());
-            statement.setString(4, user.getPasswordSalt());
+            statement.setString(++idx, user.getName());
+            statement.setString(++idx, user.getEmail());
+            statement.setString(++idx, user.getPassword());
+            statement.setString(++idx, user.getPasswordSalt());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
@@ -31,14 +32,16 @@ public class UserDAO {
             }
             return user;
         } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+            catchCode(e);
+            return user;
+        } finally {
+            closeConnection(conn);
         }
     }
 
-    public User read(int userID) {
+    public User read(int userID) throws SQLException {
+        Connection conn = getConnection();
         try {
-            Connection conn = getConnection();
             PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
             statement.setInt(1, userID);
             ResultSet rs = statement.executeQuery();
@@ -52,85 +55,112 @@ public class UserDAO {
                 user.setPasswordSalt(rs.getString("passwordSalt"));
                 return user;
             } else {
-                return null;
+                User user = new User();
+                return user;
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            catchCode(e);
             return null;
+        } finally {
+            closeConnection(conn);
         }
     }
 
-    public void update(User user) {
+    public void update(User user) throws SQLException {
+        Connection conn = getConnection();
         try {
-            Connection conn = getConnection();
+            int idx = 0;
             PreparedStatement statement = conn.prepareStatement(UPDATE_USER_QUERY);
-            statement.setString(1, user.getName());
-            statement.setString(2, user.getEmail());
-            statement.setString(3, user.getPassword());
-            statement.setString(4, user.getPasswordSalt());
-            statement.setInt(5, user.getId());
+            statement.setString(++idx, user.getName());
+            statement.setString(++idx, user.getEmail());
+            statement.setString(++idx, user.getPassword());
+            statement.setString(++idx, user.getPasswordSalt());
+            statement.setInt(++idx, user.getId());
             statement.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            catchCode(e);
+        } finally {
+            closeConnection(conn);
         }
     }
 
-    public boolean delete(int userId) {
+    public boolean delete(int userId) throws SQLException {
+        Connection conn = getConnection();
         try {
-            Connection conn = getConnection();
             PreparedStatement statement = conn.prepareStatement(DELETE_USER_QUERY);
             statement.setInt(1, userId);
             return statement.executeUpdate() == 1;
         } catch (SQLException e) {
-            e.printStackTrace();
+            catchCode(e);
             return false;
+        } finally {
+            closeConnection(conn);
         }
     }
 
-    public List<User> findAll() {
+    public List<User> findAll() throws SQLException {
+        Connection conn = getConnection();
         try {
-            Connection conn = getConnection();
             List<User> users = new ArrayList<>();
             PreparedStatement statement = conn.prepareStatement(FIND_ALL_USERS_QUERY);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setEmail(resultSet.getString("email"));
-                user.setPassword(resultSet.getString("password"));
-                user.setPasswordSalt(resultSet.getString("passwordSalt"));
-
+                User user = setUser(resultSet);
                 users.add(user);
             }
             return users;
         } catch (SQLException e) {
-            e.printStackTrace();
+            catchCode(e);
             return null;
+        } finally {
+            closeConnection(conn);
         }
     }
 
-    public User findAllByGroupId(int groupID) {
+    public User findAllByGroupId(int groupID) throws SQLException {
+        Connection conn = getConnection();
         try {
-            Connection conn = getConnection();
             PreparedStatement statement = conn.prepareStatement(FIND_ALL_GROUP_USERS_QUERY, Statement.RETURN_GENERATED_KEYS);
             statement.setInt(1, groupID);
             ResultSet rs = statement.executeQuery();
 
             if (rs.next()) {
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setEmail(rs.getString("email"));
-                user.setPassword(rs.getString("password"));
-                user.setPasswordSalt(rs.getString("passwordSalt"));
+                User user = setUser(rs);
                 return user;
             } else {
-                return null;
+                User user = new User();
+                return user;
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            catchCode(e);
             return null;
+        } finally {
+            closeConnection(conn);
         }
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                catchCode(e);
+            }
+        }
+    }
+
+    public static void catchCode(SQLException e) {
+        e.getMessage();
+        e.printStackTrace();
+    }
+
+    public static User setUser(ResultSet rs) throws SQLException {
+        User user = new User();
+        user.setId(rs.getInt("id"));
+        user.setName(rs.getString("name"));
+        user.setEmail(rs.getString("email"));
+        user.setPassword(rs.getString("password"));
+        user.setPasswordSalt(rs.getString("passwordSalt"));
+        return user;
     }
 }
